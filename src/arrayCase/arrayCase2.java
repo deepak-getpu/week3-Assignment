d0ff36b941673ec33ecdc0529f5e35fb75e62f61
deepak-getpu <deepak.getpu@gmail.com>
2023-12-02 01:54:14 -0500

    changes being made

--- src/arrayCase/arrayCase2.java
+++ src/arrayCase/arrayCase2.java
@@ -4,7 +4,7 @@ public class arrayCase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+// The test program: 
 		
 		String firstName = "Mary";
 		String lastName = "Brown";
